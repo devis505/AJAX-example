3f361132fa2d42300c743bbe62c2f4001a0c5cd7
devis505 <ziga1707@gmail.com>
2016-07-04 16:09:37 +0300

    Изучение AJAX
    Поддержка устаревших версий браузеров

---
+++ example 3 (compatibility with older browsers)/script.js
@@ -0,0 +1,22 @@
+var request;
+
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+}  else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+request.open('GET', 'data.txt', false);
+
+/* Создаём йункцию которая будет вызвана после учадного выполнения запроса */
+request.onreadystatechange = function() {
+    if ((request.readyState === 4) && (request.status === 200)) {
+        console.log(request);
+        document.writeln(request.responseText);
+    }
+}
+
+
+request.send();
+
+
